d620419ad4a2f893c672aec0cc47b608575e00e4
Chelsea Damora <damora.chelsea@gmail.com>
2022-06-12 18:11:55 -0500

    added comments to theaters service

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,6 +1,7 @@
 const knex = require("../db/connection")
 const reduceProperties = require("../utils/reduce-properties")
 
+//will dynamically calculate the array index of an array property by replacing any `null` value with the next index
 const reduceMoves = reduceProperties("theater_id", {
     m_movie_id: ["movies", null, "movie_id"],
     m_title: ["movies", null, "title"],
@@ -14,8 +15,9 @@ const reduceMoves = reduceProperties("theater_id", {
     mt_theater_id: ["movies", null, "theater_id"],
 })
 
+//lists all the theaters
 function list() {
-    // listed every single row in order to minimize ambiguity within the select function
+   
     return knex("theaters as t")
         .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
         .join("movies as m", "m.movie_id", "mt.movie_id")
