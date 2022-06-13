88dfeff3a42cc08c4514aae2f27bfcae133c2eb3
Chelsea Damora <damora.chelsea@gmail.com>
2022-06-13 04:28:34 -0500

    added comments for movies controller

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -4,10 +4,9 @@ const service = require("./movies.service")
 //returns a list of all the movies that are showing
 async function list(req, res, next){
     const { is_showing } = req.query
-
-    if (!(is_showing))
+    if (!(is_showing)){
         return res.json({ data: await service.list() })
-
+    }
     const movieList = await service.currentlyShowing()
     res.json({ data: movieList })
 }
@@ -27,12 +26,15 @@ async function movieExists(req,res,next){
 async function read(req, res, next){
     res.json({data: res.locals.movie})
 }
+
+//reads a list of theaters showing at the movieId parameter
 async function readTheaters(req, res, next) {
     const { movieId } = req.params;
     const theaterList = await service.readTheaters(movieId);
     res.json({ data: theaterList });
-  }
+}
 
+//reads the reviews that match teh movieId parameter
 async function readReviews(req, res) {
     const { movieId } = req.params;
     const reviewList = await service.readReviews(movieId);
