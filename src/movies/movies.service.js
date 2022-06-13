fe3f79463b0bd68b6fcbc58a4dfc28175ee63de1
Chelsea Damora <damora.chelsea@gmail.com>
2022-06-13 04:27:49 -0500

    added comments

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,6 +1,8 @@
 const knex = require("../db/connection") // need knex for db management
 const mapProperties = require("../utils/map-properties")
 
+
+//Creates a function to map the properties of an object to different properties on a new object.
 const criticInfo = mapProperties({
     critic_id: "critic.critic_id",
     preferred_name: "critic.preferred_name",
@@ -10,40 +12,45 @@ const criticInfo = mapProperties({
     updated_at: "critic.updated_at"
   });
   
+//reads the reviews for a movie at the movieId parameter
 function readReviews(movieId) {
     return knex("movies as m")
-    .join("reviews as r" , "r.movie_id", "m.movie_id")
-    .join("critics as c", "c.critic_id", "r.critic_id")
-    .where({"m.movie_id": movieId})
-    .select("*")
-    .then((reviews) => reviews.map(criticInfo))
+    .join("reviews as r" , "r.movie_id", "m.movie_id")//joins reviews and movies based on common primaryId
+    .join("critics as c", "c.critic_id", "r.critic_id")//joins critic table based on common reviewId
+    .where({"m.movie_id": movieId})//where teh movie id matches the movieId parameter
+    .select("*")//select all
+    .then((reviews) => reviews.map(criticInfo))//then map the reviews with the critics info properties
   }
 
+//return a list of movies from the table using knex
 function list() {
     return knex("movies").select("*")
 }
 
+//shows a list of currently showing movies from the movies table using knex
 function currentlyShowing() {
     return knex("movies as m")
-        .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
-        .select("m.*")
-        .where({ "mt.is_showing": true })
-        .groupBy("m.movie_id")
+    .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")//joinging movies and theates by a shared primary ID
+    .select("m.*")
+    .where({ "mt.is_showing": true })
+    .groupBy("m.movie_id")
 }
 
+//shows a list of theaters that are showing a movie at movieID as the parameter
 function readTheaters(movieId) {
     return knex("movies as m")
-    .join("movies_theaters as mt", "mt.movie_id", "m.movie_id")
+    .join("movies_theaters as mt", "mt.movie_id", "m.movie_id")//joining movies and theaters by a shared primary ID
     .join("theaters as t", "t.theater_id", "mt.theater_id")
     .select("t.*")
     .where({"m.movie_id": movieId})
   }
 
+//reads info of movie from the movies table with the movieId parameter
 function read(movieId) {
     return knex("movies as m")
-        .select("*")
-        .where({ "m.movie_id" : movieId })
-        .first()
+    .select("*")
+    .where({ "m.movie_id" : movieId })
+    .first()
 }
 
 
--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,16 +1,19 @@
 const knex = require("../db/connection");
 const mapProperties = require("../utils/map-properties");
 
+//Creates a function to map the properties of an object to different properties on a new object.
 const mapCritic = mapProperties({
   preferred_name: "critic.preferred_name",
   surname: "critic.surname",
   organization_name: "critic.organization_name",
 });
 
+//using knex to read what is in the review table
 function read(reviewId) {
   return knex("reviews").select("*").where({ review_id: reviewId }).first();
 }
 
+//using knex to update the review table with the updateReview info
 function update(updatedReview) {
   return knex("reviews")
     .select("*")
@@ -25,7 +28,7 @@ function update(updatedReview) {
         .then(mapCritic);
     });
 }
-
+//destroy a review from the table by the reviewId
 function destroy(reviewId) {
   return knex("reviews")
   .where({ review_id: reviewId })
